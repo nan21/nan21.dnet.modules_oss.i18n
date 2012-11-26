2d3a1db570ce3c7277e80d6a3b0c6187c54bd45f
attila.mathe <mathe_attila@yahoo.com>
2012-11-26 13:34:32 +0200

    - PI/PO/SI/SO product LOV restrict values to purchase=true or sale=true based on document type.

--- src/main/resources/en/nan21.dnet.module.md.ui.extjs/ds/ProductWithUomLovDs.js
+++ src/main/resources/en/nan21.dnet.module.md.ui.extjs/ds/ProductWithUomLovDs.js
@@ -1,5 +1,7 @@
 Ext.define("net.nan21.dnet.module.md.mm.prod.ds.model.ProductWithUomLovDs$Trl", {
 	
+	purchase__lbl: "Purchase",
+	sale__lbl: "Sale",
 	uomId__lbl: "Uom(ID)",
 	uom__lbl: "Uom"
 });
--- src/main/resources/ro/nan21.dnet.module.md.ui.extjs/ds/ProductWithUomLovDs.js
+++ src/main/resources/ro/nan21.dnet.module.md.ui.extjs/ds/ProductWithUomLovDs.js
@@ -1,5 +1,7 @@
 Ext.define("net.nan21.dnet.module.md.mm.prod.ds.model.ProductWithUomLovDs$Trl", {
 	
+	purchase__lbl: "Cumpărare",
+	sale__lbl: "Vânzare",
 	uomId__lbl: "UM(ID)",
 	uom__lbl: "UM"
 });
